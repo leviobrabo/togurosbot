ed0c54b90df68412893e77e28bb6dae1c9cff951
leviobrabo <carlosjunior20313@gmail.com>
2023-04-21 16:20:25 -0300

    update groups list

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -419,7 +419,7 @@ async function groups(message) {
 
         for (let chat of chats) {
             if (chat.chatId < 0) {
-                let groupMessage = `*${contador}:* *Group:* <a href="tg://resolve?domain=${chat.chatName}&amp;id=${chat.chatId}">${chat.chatName}</a> *ID:* <code>${chat.chatId}</code>\n`;
+                let groupMessage = `<b>${contador}:</b> <b>Group:</b> <a href="tg://resolve?domain=${chat.chatName}&amp;id=${chat.chatId}">${chat.chatName}</a> *ID:* <code>${chat.chatId}</code>\n`;
                 if (currentChunk.length + groupMessage.length > chunkSize) {
                     messageChunks.push(currentChunk);
                     currentChunk = "";
