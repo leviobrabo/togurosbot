e69a1276292e761a018a9a848afe5bef472aec64
leviobrabo <carlosjunior20313@gmail.com>
2023-04-21 16:47:20 -0300

    update group list 2.6

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -400,6 +400,8 @@ async function stats(message) {
     }
 }
 
+const { ChatModel } = require("../database");
+
 async function groups(message) {
     const user_id = message.from.id;
     if (!(await is_dev(user_id))) {
@@ -413,13 +415,13 @@ async function groups(message) {
         const chats = await ChatModel.find().sort({ chatId: 1 });
 
         let contador = 1;
-        let chunkSize = 4096;
-        let messageChunks = [""];
+        let chunkSize = 4096; // alterado o chunkSize para 4096
+        let messageChunks = [];
         let currentChunk = "";
 
         for (let chat of chats) {
             if (chat.chatId < 0) {
-                let groupMessage = `<b>${contador}:</b> <b>Group:</b> <a href="tg://resolve?domain=${chat.chatName}&amp;id=${chat.chatId}">${chat.chatName}</a> *ID:* <code>${chat.chatId}</code>\n`;
+                let groupMessage = `*${contador}:* *Group:* <a href="tg://resolve?domain=${chat.chatName}&amp;id=${chat.chatId}">${chat.chatName}</a> *ID:* <code>${chat.chatId}</code>\n`;
                 if (currentChunk.length + groupMessage.length > chunkSize) {
                     messageChunks.push(currentChunk);
                     currentChunk = "";
@@ -430,48 +432,49 @@ async function groups(message) {
         }
         messageChunks.push(currentChunk);
 
-        let index = 0;
-        const markup = {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {
-                            text: `<<`,
-                            callback_data: `groups:${index - 1}`,
-                            disabled: index === 0,
-                        },
-                        {
-                            text: `>> ${index + 2}`,
-                            callback_data: `groups:${index + 1}`,
-                            disabled: index === messageChunks.length - 1,
-                        },
-                    ],
-                ],
-            },
-            parse_mode: "HTML",
-        };
-
-        if (messageChunks.length === 1) {
-            delete markup.reply_markup.inline_keyboard;
+        // adicionando botões de avançar e voltar
+        let buttons = [];
+        if (messageChunks.length > 1) {
+            if (messageChunks.indexOf(currentChunk) > 0) {
+                buttons.push({
+                    text: "Voltar",
+                    callback_data: "back",
+                });
+            }
+            if (
+                messageChunks.indexOf(currentChunk) <
+                messageChunks.length - 1
+            ) {
+                buttons.push({
+                    text: "Avançar",
+                    callback_data: "next",
+                });
+            }
         }
 
-        await bot.sendMessage(message.chat.id, messageChunks[index], markup);
+        // adicionando botões ao último chunk
+        let lastChunk = messageChunks[messageChunks.length - 1];
+        let options = {
+            parse_mode: "HTML",
+            reply_markup: {
+                inline_keyboard: [buttons],
+            },
+        };
 
-        bot.on("callback_query", async (query) => {
-            if (query.data.startsWith("groups:")) {
-                index = Number(query.data.split(":")[1]);
-                markup.reply_markup.inline_keyboard[0][0].disabled =
-                    index === 0;
-                markup.reply_markup.inline_keyboard[0][1].disabled =
-                    index === messageChunks.length - 1;
-                await bot.editMessageText(messageChunks[index], {
-                    chat_id: query.message.chat.id,
-                    message_id: query.message.message_id,
-                    ...markup,
-                });
-                await bot.answerCallbackQuery(query.id);
-            }
-        });
+        await bot.sendMessage(message.chat.id, lastChunk, options);
+
+        // adicionando botões aos demais chunks
+        for (let i = 0; i < messageChunks.length - 1; i++) {
+            let options = {
+                parse_mode: "HTML",
+                reply_markup: {
+                    inline_keyboard: [
+                        [{ text: "Avançar", callback_data: "next" }],
+                    ],
+                },
+            };
+            await bot.sendMessage(message.chat.id, messageChunks[i], options);
+        }
     } catch (error) {
         console.error(error);
     }
