3ae0076f1e1d58ed2b1c48c12cf14cd97a1620a1
leviobrabo <carlosjunior20313@gmail.com>
2023-04-22 00:47:47 -0300

    udpate

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -202,6 +202,11 @@ async function answerUser(message) {
     const receivedMessage = message.sticker?.file_unique_id ?? message.text;
     const chatId = message.chat.id;
 
+    const regex = /^[\/.!]/;
+    if (regex.test(receivedMessage)) {
+        return;
+    }
+
     const sendMessageOptions = { reply_to_message_id: message.message_id };
 
     const audioMatch = audioList.find((audio) =>
