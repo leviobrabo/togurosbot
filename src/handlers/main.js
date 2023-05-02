bbd4033c354750dd11bcfb796a4da3743a379e98
leviobrabo <carlosjunior20313@gmail.com>
2023-05-02 18:47:23 -0300

    update bot

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -173,6 +173,11 @@ async function answerUser(message) {
     const receivedMessage = message.sticker?.file_unique_id ?? message.text;
     const chatId = message.chat.id;
 
+    const regex = /^[\/.!]/;
+    if (regex.test(receivedMessage)) {
+        return;
+    }
+
     const sendMessageOptions = { reply_to_message_id: message.message_id };
 
     const audioMatch = audioList.find((audio) =>
@@ -241,10 +246,6 @@ async function saveUserInformation(message) {
 async function main(message) {
     const replyToMessage = message?.reply_to_message ?? false;
     const { id: botId } = await bot.getMe();
-
-    if (message.chat.type === "private") {
-        await saveUserInformation(message);
-    }
 
     if (message.sticker || message.text) {
         if (replyToMessage && replyToMessage.from.id != botId)
