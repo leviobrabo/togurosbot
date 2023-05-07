2db994483c8a1be53b2216826346d5203d0f19e2
leviobrabo <carlosjunior20313@gmail.com>
2023-05-07 12:52:59 -0300

    adicionado nº de usuario e chat do bot no status

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -919,8 +919,10 @@ async function sendStatus() {
     const m_s = end - start;
     const uptime = process.uptime();
     const uptime_formatted = timeFormatter(uptime);
+    const numUsers = await UserModel.countDocuments();
+    const numChats = await ChatModel.countDocuments();
     await bot.editMessageText(
-        `#Togurosbot #Status\n\nStatus: ON\nPing: \`${m_s}ms\`\nUptime: \`${uptime_formatted}\``,
+        `#Togurosbot #Status\n\nStatus: ON\nPing: \`${m_s}ms\`\nUptime: \`${uptime_formatted}\`\nUsers: \`${numUsers}\`\nChats: \`${numChats}\``,
         {
             chat_id: replied.chat.id,
             message_id: replied.message_id,
